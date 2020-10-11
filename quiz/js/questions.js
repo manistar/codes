4101383b33e95ef5332208d7006b843437dd1942
Ayokunle Vincent <59540587+manistar@users.noreply.github.com>
2020-10-11 19:13:14 +0100

    Add files via upload

---
+++ quiz/js/questions.js
@@ -0,0 +1,243 @@
+//creating an array and passing the number, questions, option, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Markup Languages",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Cascading Style Sheets",
+            "Computer Style Sheet",
+            "Cascading Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP stand for?",
+        answer: "Hypertext Preprocessor",
+        options: [
+            "Hypertext Preprocessor",
+            "Hypertext Preprogramming",
+            "Hypertext Processor",
+            "Hypertext Processing",
+            "Hometext Preprocessor"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: "Structured Query Language",
+        options: [
+            "Stylish Question Language",
+            "Stylesheet Query Language",
+            "Statement Question Language",
+            "Structural Query Language",
+            "Structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "eXtensible Markup Language",
+        options: [
+            "eXtensible Markup Language",
+            "eXecutable Multiple Language",
+            "eXtra Multi-Program Language",
+            "eXtra Multi-Programming Language",
+            "eXamine Multiple Language"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Which tag contains the visual part of the web page?",
+        answer: "Body",
+        options: [
+            "Head",
+            "Footer",
+            "!DOCTYPE",
+            "Body",
+            "HTML"
+        ]
+    },
+    {
+        numb: 7,
+        question: "When formatting text, can you get the same result when using different tags?",
+        answer: "Yes",
+        options: [
+            "Probably",
+            "Sometimes",
+            "Not Sure",
+            "Yes",
+            "No"
+        ]
+    },
+    {
+        numb: 8,
+        question: "What does the href attribute contain?",
+        answer: "The URL of the page to be transferred",
+        options: [
+            "Whether the new page should be opened in the same or a new window",
+            "The name of the web page to be transferred",
+            "The URL of the page to be transferred",
+            "I don't Know",
+            "The web UR'L of the page to be transferred"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Which tag contains the cell tags besides the table tag?",
+        answer: "tr",
+        options: [
+            "ul",
+            "ol",
+            "ul",
+            "tr",
+            "table"
+        ]
+    },
+     {
+        numb: 10,
+        question: "Choose the correct HTML tag for the largest heading:",
+        answer: "h1",
+        options: [
+            "head",
+            "h1",
+            "h6",
+            "heading",
+            "header"
+        ]
+    },
+    {
+        numb: 11,
+        question: "Which of these tags are all table tags?",
+        answer: "table.tr.td",
+        options: [
+            "thead.body.tr",
+            "table.tr.tt",
+            "table.tr.td",
+            "tr.tt.dd",
+            "tr.ttd.dd"
+        ]
+    },
+     {
+        numb: 12,
+        question: 'Is width="100" and width="100%" the same?',
+        answer: "No",
+        options: [
+            "Yes",
+            "No",
+            "Not Sure",
+            "Can't Say",
+            "I Don't Know"
+        ]
+    },
+      {
+        numb: 13,
+        question: "Which choice is the correct HTML5 element for playing video files?",
+        answer: "video",
+        options: [
+            "movie",
+            "media",
+            "video",
+            "image",
+            "canva"
+        ]
+    },
+    {
+        numb: 14,
+        question: "The canvas element in HTML5 is used to:",
+        answer: "Draw graphics",
+        options: [
+            "Manipulate data in a database",
+            "Display Excel records",
+            "Draw graphics",
+            "Create draggable elements",
+            "Create & drag elements"
+        ]
+    },
+    {
+        numb: 15,
+        question: "sessionStorage stores data for the duration of how many session(s)?",
+        answer: "One",
+        options: [
+            "Three",
+            "One",
+            "Two",
+            "Multiple",
+            "Infinit"
+        ]
+    },
+    {
+        numb: 16,
+        question: "What does a markup language use to identify content?",
+        answer: "tags",
+        options: [
+            "scripts",
+            "commands",
+            "tags",
+            "functions",
+            "meta description"
+        ]
+    },
+    {
+        numb: 17,
+        question: "What is the correct extension for HTML files?",
+        answer: ".html",
+        options: [
+            ".css",
+            ".exe",
+            ".txt",
+            ".html",
+            ".js"
+        ]
+    },
+    {
+        numb: 18,
+        question: "What tag is used to create a line-break without an extra space between the text blocks?",
+        answer: "br",
+        options: [
+            "linebreak",
+            "break",
+            "ib",
+            "br",
+            "brr"
+        ]
+    },
+    {
+        numb: 19,
+        question: "Which two tags below make the text visually bold?",
+        answer: "b and strong",
+        options: [
+            "sub and sup",
+            "i and em",
+            "b and strong",
+            "ins and del",
+            "bold and em"
+        ]
+    },
+    {
+        numb: 20,
+        question: "What tags are used to indicate headings?",
+        answer: "h1 - h6",
+        options: [
+            "head",
+            "header",
+            "h1 - h6",
+            "heading",
+            "header"
+        ]
+    },
+];
